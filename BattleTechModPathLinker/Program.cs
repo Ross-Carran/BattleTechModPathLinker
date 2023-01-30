a284dbdf4e27613a4fa885d8b06dea37176e9f7c
Ross Carran <carran.ross@gmail.com>
2023-01-30 17:45:18 +1300

    Basic Functioning Symbolic linking of folders to destination. No current documentation, only processes first two arguments. Argument One: Destination Directory Argument Two: Root folder of subfolders to Symbolic link

--- BattleTechModPathLinker/Program.cs
+++ BattleTechModPathLinker/Program.cs
@@ -1,3 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
+namespace BattleTechModPathLinker;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        bool hasTwoArgs = args.Length >= 2;
+        Console.WriteLine("Hello, World!");
+        switch (hasTwoArgs)
+        {
+            case true:
+            {
+                string[] basePath = Directory.GetDirectories(args[1]);
+
+                foreach (var dir in basePath)
+                {
+                    MakeSymbolicLinks(args[0],dir);
+                    Console.WriteLine(dir);
+                }
+            }
+                break;
+        }
+    }
+    private static void MakeSymbolicLinks(string destination, string source)
+    {
+        try
+        {
+            Directory.CreateSymbolicLink(destination + Path.DirectorySeparatorChar + new DirectoryInfo(source).Name, source);
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+
